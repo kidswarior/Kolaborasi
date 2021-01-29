6af244de5de5e672181fc90976490f2ee7477b8c
Adam7RPL <adamganteng598@gmail.com>
2021-01-29 14:28:09 +0700

    tahap kolaborasi

---
+++ app/src/main/java/com/alifproduction/kolaborasi/AdaamActivity.java
@@ -0,0 +1,14 @@
+package com.alifproduction.kolaborasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdaamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adaam);
+    }
+}
